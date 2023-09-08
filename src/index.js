82895f08e47d0d62a3f509aa6e9e9804479a1f95
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-09-08 23:53:19 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreed } from './api';
@@ -8,29 +9,33 @@ const loader = document.querySelector('.loader-wrapper');
 
 const error = document.querySelector('.error');
 
-loader.style.display = 'none';
 error.style.display = 'none';
+select.style.opacity = '0';
 
-fetchBreeds().then(res => {
-  console.log(res.data);
+fetchBreeds()
+  .then(res => {
+    select.style.opacity = '1';
 
-  const options = res.data;
-  const markup = options
-    .map(item => `<option value="${item.id}">${item.name}</option>`)
-    .join('');
-  select.insertAdjacentHTML('beforeend', markup);
-  new SlimSelect({
-    select: '#single',
-  });
-});
+    const options = res.data;
+    const markup = options
+      .map(item => `<option value="${item.id}">${item.name}</option>`)
+      .join('');
+    select.insertAdjacentHTML('beforeend', markup);
+    new SlimSelect({
+      select: '#single',
+    });
+  })
+  .catch(error => {
+    Notiflix.Notify.failure(error.message);
+  })
+  .finally(() => (loader.style.display = 'none'));
 
 select.addEventListener('change', () => {
   loader.style.display = 'flex';
   const id = select.options[select.selectedIndex].value;
-  fetchCatByBreed(id).then(res => {
-    loader.style.display = 'none';
-    console.log(res.data);
-    const infoMarkup = `<div class="container">
+  fetchCatByBreed(id)
+    .then(res => {
+      const infoMarkup = `<div class="container">
     <img src="${res.data[0].url}" width="400px">
     <div class="container-description">
     <h1>${res.data[0].breeds[0].name}</h1>
@@ -38,6 +43,10 @@ select.addEventListener('change', () => {
     <h2>Temperament</h2>
     <span class="temperament">${res.data[0].breeds[0].temperament}</span>
     </div></div>`;
-    info.insertAdjacentHTML('afterend', infoMarkup);
-  });
+      info.innerHTML = infoMarkup;
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(error.message);
+    })
+    .finally(() => (loader.style.display = 'none'));
 });
